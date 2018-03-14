e016ee58bf36843494c13ec38966222088b61305
BlueStriker40 <36478881+BlueStriker40@users.noreply.github.com>
2018-03-14 08:33:01 -0400

    Update hilitor.js

--- hilitor.js
+++ hilitor.js
@@ -1,233 +1,154 @@
-function Hilitor2(id, tag)
-{
-
-  var targetNode = document.getElementById(id) || document.body;
-  var hiliteTag = tag || "EM";
-  var skipTags = new RegExp("^(?:" + hiliteTag + "|SCRIPT|FORM)$");
-  var colors = ["#ff6", "#a0ffff", "#9f9", "#f99", "#f6f"];
-  var wordColor = [];
-  var colorIdx = 0;
-  var matchRegex = "";
-  var openLeft = false;
-  var openRight = false;
-
-  this.setMatchType = function(type)
-  {
-    switch(type)
-    {
-      case "left":
-        this.openLeft = false;
-        this.openRight = true;
-        break;
-
-      case "right":
-        this.openLeft = true;
-        this.openRight = false;
-        break;
-
-      case "open":
-        this.openLeft = this.openRight = true;
-        break;
-
-      default:
-        this.openLeft = this.openRight = false;
-
-    }
-  };
-
-  function addAccents(input)
-  {
-    retval = input;
-    retval = retval.replace(/([ao])e/ig, "$1");
-    retval = retval.replace(/\\u00E[024]/ig, "a");
-    retval = retval.replace(/\\u00E7/ig, "c");
-    retval = retval.replace(/\\u00E[89AB]/ig, "e");
-    retval = retval.replace(/\\u00E[EF]/ig, "i");
-    retval = retval.replace(/\\u00F[46]/ig, "o");
-    retval = retval.replace(/\\u00F[9BC]/ig, "u");
-    retval = retval.replace(/\\u00FF/ig, "y");
-    retval = retval.replace(/\\u00DF/ig, "s");
-    retval = retval.replace(/a/ig, "([aàâä]|ae)");
-    retval = retval.replace(/c/ig, "[cç]");
-    retval = retval.replace(/e/ig, "[eèéêë]");
-    retval = retval.replace(/i/ig, "[iîï]");
-    retval = retval.replace(/o/ig, "([oôö]|oe)");
-    retval = retval.replace(/u/ig, "[uùûü]");
-    retval = retval.replace(/y/ig, "[yÿ]");
-    retval = retval.replace(/s/ig, "(ss|[sß])");
-    return retval;
-  }
-
-  this.setRegex = function(input)
-  {
-    input = input.replace(/\\([^u]|$)/g, "$1");
-    input = input.replace(/[^\w\\\s']+/g, "").replace(/\s+/g, "|");
-    input = input.replace(/^\||\|$/g, "");
-    input = addAccents(input);
-    if(input) {
-      var re = "(" + input + ")";
-      if(!this.openLeft) re = "(?:^|[\\b\\s])" + re;
-      if(!this.openRight) re = re + "(?:[\\b\\s]|$)";
-      matchRegex = new RegExp(re, "i");
-      return true;
-    }
-    return false;
-  };
-
-  this.getRegex = function()
-  {
-    var retval = matchRegex.toString();
-    retval = retval.replace(/(^\/|\(\?:[^\)]+\)|\/i$)/g, "");
-    return retval;
-  };
-
-  // recursively apply word highlighting
-  this.hiliteWords = function(node)
-  {
-    if(node === undefined || !node) return;
-    if(!matchRegex) return;
-    if(skipTags.test(node.nodeName)) return;
-
-    if(node.hasChildNodes()) {
-      for(var i=0; i < node.childNodes.length; i++)
-        this.hiliteWords(node.childNodes[i]);
-    }
-    if(node.nodeType == 3) { // NODE_TEXT
-      if((nv = node.nodeValue) && (regs = matchRegex.exec(nv))) {
-        if(!wordColor[regs[1].toLowerCase()]) {
-          wordColor[regs[1].toLowerCase()] = colors[colorIdx++ % colors.length];
-        }
-
-        var match = document.createElement(hiliteTag);
-        match.appendChild(document.createTextNode(regs[1]));
-        match.style.backgroundColor = wordColor[regs[1].toLowerCase()];
-        match.style.fontStyle = "inherit";
-        match.style.color = "#000";
-
-        var after;
-        if(regs[0].match(/^\s/)) { // in case of leading whitespace
-          after = node.splitText(regs.index + 1);
-        } else {
-          after = node.splitText(regs.index);
-        }
-        after.nodeValue = after.nodeValue.substring(regs[1].length);
-        node.parentNode.insertBefore(match, after);
-      }
-    };
-  };
-
-  // remove highlighting
-  this.remove = function()
-  {
-    var arr = document.getElementsByTagName(hiliteTag);
-    while(arr.length && (el = arr[0])) {
-      var parent = el.parentNode;
-      parent.replaceChild(el.firstChild, el);
-      parent.normalize();
-    }
-  };
-
-  // start highlighting at target node
-  this.apply = function(input)
-  {
-    this.remove();
-    if(input === undefined || !(input = input.replace(/(^\s+|\s+$)/g, ""))) return;
-    input = convertCharStr2jEsc(input);
-    if(this.setRegex(input)) {
-      this.hiliteWords(targetNode);
-    }
-  };
-
-  // added by Yanosh Kunsh to include utf-8 string comparison
-  function dec2hex4(textString)
-  {
-    var hexequiv = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F");
-    return hexequiv[(textString >> 12) & 0xF] + hexequiv[(textString >> 8) & 0xF] + hexequiv[(textString >> 4) & 0xF] + hexequiv[textString & 0xF];
-  }
-
-  function convertCharStr2jEsc(str, cstyle)
-  {
-    // Converts a string of characters to JavaScript escapes
-    // str: sequence of Unicode characters
-    var highsurrogate = 0;
-    var suppCP;
-    var pad;
-    var n = 0;
-    var outputString = '';
-    for(var i=0; i < str.length; i++) {
-      var cc = str.charCodeAt(i);
-      if(cc < 0 || cc > 0xFFFF) {
-        outputString += '!Error in convertCharStr2UTF16: unexpected charCodeAt result, cc=' + cc + '!';
-      }
-      if(highsurrogate != 0) { // this is a supp char, and cc contains the low surrogate
-        if(0xDC00 <= cc && cc <= 0xDFFF) {
-          suppCP = 0x10000 + ((highsurrogate - 0xD800) << 10) + (cc - 0xDC00);
-          if(cstyle) {
-            pad = suppCP.toString(16);
-            while(pad.length < 8) {
-              pad = '0' + pad;
-            }
-            outputString += '\\U' + pad;
-          } else {
-            suppCP -= 0x10000;
-            outputString += '\\u' + dec2hex4(0xD800 | (suppCP >> 10)) + '\\u' + dec2hex4(0xDC00 | (suppCP & 0x3FF));
-          }
-          highsurrogate = 0;
-          continue;
-        } else {
-          outputString += 'Error in convertCharStr2UTF16: low surrogate expected, cc=' + cc + '!';
-          highsurrogate = 0;
-        }
-      }
-      if(0xD800 <= cc && cc <= 0xDBFF) { // start of supplementary character
-        highsurrogate = cc;
-      } else { // this is a BMP character
-        switch(cc)
-        {
-          case 0:
-            outputString += '\\0';
-            break;
-          case 8:
-            outputString += '\\b';
-            break;
-          case 9:
-            outputString += '\\t';
-            break;
-          case 10:
-            outputString += '\\n';
-            break;
-          case 13:
-            outputString += '\\r';
-            break;
-          case 11:
-            outputString += '\\v';
-            break;
-          case 12:
-            outputString += '\\f';
-            break;
-          case 34:
-            outputString += '\\\"';
-            break;
-          case 39:
-            outputString += '\\\'';
-            break;
-          case 92:
-            outputString += '\\\\';
-            break;
-          default:
-            if(cc > 0x1f && cc < 0x7F) {
-              outputString += String.fromCharCode(cc);
-            } else {
-              pad = cc.toString(16).toUpperCase();
-              while(pad.length < 4) {
-                pad = '0' + pad;
-              }
-              outputString += '\\u' + pad;
-            }
-        }
-      }
-    }
-    return outputString;
-  }
-
+<!DOCTYPE html>
+<head>
+<script src="/assets/jquery.js"></script>
+<link href='https://media.glassdoor.com/l/153054/red-lobster-office.jpg' rel='stylesheet' type='text/css'>
+<style>
+body {
+  background: url("https://media.glassdoor.com/l/153054/red-lobster-office.jpg");
+  font-family: helvetica, sans-serif;
+  margin: 0 auto;
+  max-width: 600px;
 }
+div {
+  height: 200px;
+  background-size: cover;
+  position: relative;
+  margin: 40px 0 0 0;
+  border-radius: 12px;
+}
+h1 {
+  font-family: 'Londrina Shadow', cursive;
+  text-align: center;
+  font-size: 75px;
+  color: #aaaaaa;
+  margin: 60px 0 0 0;
+}
+h2 {
+  text-align: center;
+  color: #bbbbbb;
+  margin: 0px 0 70px 0;
+}
+p {
+  color: rgba(255,255,255,1);
+  background: black;
+  background: linear-gradient(bottom, rgba(0,0,0,1), rgba(0,0,0,.4));
+  background: -webkit-linear-gradient(bottom, rgba(0,0,0,1), rgba(0,0,0,.4));
+  background: -moz-linear-gradient(bottom, rgba(0,0,0,1), rgba(0,0,0,.4));
+  padding: 10px;
+  line-height: 28px;
+  text-align: justify;
+  position: absolute;
+  bottom: 0;
+  margin: 0;
+  height: 30px;
+  transition: height .5s;
+  -webkit-transition: height .5s;
+  -moz-transition: height .5s;
+}
+
+small {
+  opacity: 0;
+}
+
+.show-description p {
+  height: 150px;
+}
+
+.show-description small {
+  opacity: 1;
+}
+
+.first{
+  background-image: url("https://1.bp.blogspot.com/-MXQYY8y5v-8/V47xrhzJNoI/AAAAAAAAwiw/-pph6zjnt14QWeU6-DBxbCtro2dXluO-wCLcB/s1600/red-lobster-bairdi-crab-legs.jpg");
+}
+.second{
+  background-image: url("https://www.visitgreensboronc.com/wp-content/uploads/2017/06/ultimate_feast.jpg");
+}
+.dessert{
+  background-image: url("https://3.bp.blogspot.com/-6w4-oW4hbJI/VsBD65X5mfI/AAAAAAAAtLo/BYs8x0VFZH4/s640/red-lobster-lobster-lovers-dream.jpg");
+}
+.price {
+  float: right;
+}
+@media (max-width: 500px) {
+  h1 {
+    font-size: 50px;
+    margin-top: 20px;
+    line-height: 40px;
+  }
+  h2 {
+    font-size: 20px;
+    margin: 20px 0 30px 0;
+  }
+  div {
+    margin: 20px 12px 0 12px;
+  }
+  p {
+    font-size: 20px;
+    line-height: 24px;
+  }
+  small {
+    font-size: 16px;
+  }
+}
+.jumbotron {
+     background-color: #f4511e;
+     color: #fff;
+     padding: 100px 25px;
+ }
+ .container-fluid {
+     padding: 60px 50px;
+ }
+ .bg-grey {
+     background-color: #f6f6f6;
+ }
+ .logo-small {
+     color: #f4511e;
+     font-size: 50px;
+ }
+ .logo {
+     color: #f4511e;
+     font-size: 200px;
+ }
+</style>
+
+</head>
+
+<body>
+<h1>Red lobster</h1>
+<h2>One of the best sea food restaurant in New York</h2>
+<div class="first">
+  <p>Red lobster legs only. <span class="price">$5</span><br />
+  <small>Mustard sierra leone bologi kale chard beet greens black-eyed pea sorrel amaranth garlic tigernut spring onion summer purslane asparagus lentil. </small></p>
+</div>
+
+<div class="second">
+  <p>Lobster legs with fried shrimp. <span class="price">$22</span><br />
+  <small>Tri-tip capicola kielbasa salami brisket chicken rump strip steak drumstick. Meatloaf chuck boudin ribeye pork jowl. Andouille bacon jowl meatloaf pork loin prosciutto bresaola.</small></p>
+</div>
+  
+<div class="dessert">
+  <p>Pasta with lobster meat next to it. <span class="price">$14</span><br />
+  <small>Lollipop tart cotton candy jelly-o carrot cake apple pie cupcake. Jelly-o bear claw ice cream candy canes.</small></p>
+</div>
+
+<script>
+  $('div').on('click', function() {
+      $(this).toggleClass('show-description');
+  });
+</script>
+
+</body>
+<div class="jumbotron text-center">
+  <h1>Company</h1> 
+  <p>We specialize in blablabla</p> 
+  <form class="form-inline">
+    <div class="input-group">
+      <input type="email" class="form-control" size="50" placeholder="Email Address" required>
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-danger">Subscribe</button>
+      </div>
+    </div>
+  </form>
+</div>
+
